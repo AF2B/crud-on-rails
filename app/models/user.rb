4dd5a28c5243beeca3a799875f9ffd186d9238c9
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-07-26 17:49:10 -0300

    atualizado rg para cnpj

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord
-  attr_accessor :name, :email, :dt_of_birth, :cpf, :rg, :address, :city, :state, :phone
+  attr_accessor :name, :email, :dt_of_birth, :cpf, :cnpj, :address, :city, :state, :phone
 
   validates :my_email_attribute, email: {mode: :strict, require_fqdn: true}
 end
