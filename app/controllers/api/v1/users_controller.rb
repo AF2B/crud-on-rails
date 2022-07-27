f7af2626dee44c8f36a0a4eb5be1b10b21ccddea
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-07-27 15:22:16 -0300

    controller refatorado

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,63 @@
+class Api::V1::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  def show
+    @user = -> { set_user }.call
+
+    if @user
+      render json: @user
+    else
+      render json: { error: 'User not found' }, status: 404
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user
+      @user.save
+      render json: @user
+    else
+      render json: { error: 'User not created' }, status: 422
+    end
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+
+    if @user
+      @user.update(user_params)
+      render json: @user, status: 201, message: 'User updated'
+    else
+      render json: { error: 'User not found' }, status: 404
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+
+    if @user
+      @user.destroy
+      render json: { message: 'User deleted' }
+    else
+      render json: { error: 'User not found' }, status: 404
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+    @user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :cpf, :phone)
+  end
+
+end
