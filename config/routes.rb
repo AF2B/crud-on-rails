d7e95b462e3966e65bf3f9f11450daaf06381dff
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-07-27 15:22:53 -0300

    routes refatorado

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: %i[index show create update destroy]
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
 end
