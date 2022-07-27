467d635d53305975be923ae0852379794c46e824
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-07-26 23:50:46 -0300

    user ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
-class User < ActiveRecord
-  attr_accessor :name, :email, :dt_of_birth, :cpf, :cnpj, :address, :city, :state, :phone
-
-  validates :my_email_attribute, email: {mode: :strict, require_fqdn: true}
+class User < ApplicationRecord
+  validates my_email_attribute, email: {mode: :strict, require_fqdn: true}
 end
