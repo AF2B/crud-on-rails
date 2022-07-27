79bb61a109fc5eee716d8aa5ff3eb1fd5d9dfaf8
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-07-27 15:24:02 -0300

    testes da controller iniciado

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,26 @@
 require 'rspec_helper'
 
-describe UsersController do
-  context 'when we try to get all users' do
-    it 'should return all users' do
+describe Api::V1::UsersController do
+  context 'If @users is empty' do
+    it 'should return a message' do
       get :index
-      expect(response).to have_http_status(:ok)
+      expect(response.body).to eq('{"message":"No users found"}')
+    end
+  end
+end
+
+describe Api::V1::UsersController do
+  context 'If @users is not empty' do
+    it 'should return a list of users' do
+    end
+  end
+end
+
+describe Api::V1::UsersController do
+  context 'If @user is nil' do
+    it 'should return a message' do
+      get :show, params: { id: 1 }
+      expect(response.body).to eq('{"error":"User not found"}')
     end
   end
 end
