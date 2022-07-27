2136d6c33b42c03db1531a8db8bd056dc8479c6a
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-07-26 23:52:49 -0300

    modelo bd:users

---
+++ db/migrate/20220727011258_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :cpf, null: false
+      t.string :cnpj
+      t.string :dt_of_birth
+      t.string :address
+      t.string :city
+      t.string :country
+      t.string :phone, null: false
+
+      t.timestamps
+    end
+  end
+end
