2b9972c203f902d8347172d35c05318e4e9176cb
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-08-12 16:53:28 -0300

    schema atualizado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_27_011258) do
+ActiveRecord::Schema.define(version: 2022_08_12_193932) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -21,9 +21,10 @@ ActiveRecord::Schema.define(version: 2022_07_27_011258) do
     t.string "address"
     t.string "city"
     t.string "country"
-    t.string "phone"
+    t.string "phone", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
   end
 
 end
