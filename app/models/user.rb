60283e7f8b9b8b446eab8aea29d12858e107a4dd
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-08-12 16:53:15 -0300

    adicionado validações para User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,38 @@
 class User < ApplicationRecord
+  include BCrypt
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
   validates_email_realness_of :email
+
+  def self.phone_number_valid?(phone) {
+    regex = "/(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})/"
+    result = phone.match(regex)
+    result ? true : false
+  }
+
+  def create_user(email, password)
+    user = User.new(email: email, password: password)
+    user.password = BCrypt::Password.create(password)
+    user.save!
+    user
+  end
+
+  def check_password(password)
+    BCrypt::Password.new(self.password) == password
+  end
+
+  def check_user(email, password)
+    user = find_by_email(email)
+    if user && user.check_password(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def find_by_email(email)
+    User.find_by(email: email)
+  end
 end
